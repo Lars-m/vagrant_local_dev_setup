2c1560df13ae040bcb766f16983635b9b58ac245
lars-m <lam@cphbusiness.dk>
2019-07-02 17:29:53 +0200

    mysql

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,46 @@
-apt-get update
+
+
+echo "##############################################################################"
+echo "###########                     Setup MySql 8.0              ################"
+echo "##############################################################################"
+
+export DEBIAN_FRONTEND="noninteractive";
+sudo apt-get update
+sudo apt-get install -y debconf-utils 
+sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-8.0'
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+
+sudo -E dpkg -i mmysql-apt-config_0.8.13-1_all.deb
+sudo apt-get update
+
+# Install MySQL 8
+echo "Installing MySQL 8..."
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password root'
+sudo debconf-set-selections <<< 'myvsql-community-server mysql-community-server/root-pass password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo -E apt-get -y install mysql-server
+
+# Override any existing bind-address to be 0.0.0.0 to accept connections from host
+echo "Updating my.cnf..."
+sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+echo "[mysqld]" | sudo tee -a /etc/mysql/my.cnf
+echo "bind-address=0.0.0.0" | sudo tee -a /etc/mysql/my.cnf
+echo "default-time-zone='+00:00'" | sudo tee -a /etc/mysql/my.cnf
+
+echo "Granting root access via any IP..."
+MYSQL_PWD=root mysql -u root -e "CREATE USER 'root'@'%' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+
+# Start MySQL server
+echo "Restarting MySQL..."
+sudo service mysql restart
+
+
+echo "########################## Install Java     #########################"
 sudo -E apt install -y openjdk-8-jre
+
+echo ""
+echo "########################## Tomcat Setup     #########################"
 
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
@@ -23,13 +64,12 @@ echo "##########################################################################
 sudo rm /opt/tomcat/conf/tomcat-users.xml
 sudo cat <<- EOF_TCU > /opt/tomcat/conf/tomcat-users.xml
 <?xml version="1.0" encoding="UTF-8"?>
-
 <tomcat-users xmlns="http://tomcat.apache.org/xml"
               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
               xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
               version="1.0">
 <!--
-  NOTE:  DO NOT USE THIS FILE IN PRODUCTION.
+         NOTE:  DO NOT USE THIS FILE IN PRODUCTION.
          IT'S MEANT ONLY FOR A LOCAL DEVELOPMENT SERVER USED BY NETBEANS
 -->
   <user username="netbeans" password="xf87rE" roles="manager-gui"/>
@@ -45,13 +85,33 @@ echo "##########################################################################
 
 sudo rm /opt/tomcat/webapps/manager/META-INF/context.xml
 sudo cat <<- EOF_CONTEXT > /opt/tomcat/webapps/manager/META-INF/context.xml
- <?xml version="1.0" encoding="UTF-8"?>
- <Context antiResourceLocking="false" privileged="true" >
-   <!--<Valve className="org.apache.catalina.valves.RemoteAddrValve"
-          allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />-->
-   <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
- </Context>
+<?xml version="1.0" encoding="UTF-8"?>
+<Context antiResourceLocking="false" privileged="true" >
+  <!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+</Context>
 EOF_CONTEXT
+
+sudo rm /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sudo cat <<- EOF_CONTEXT_H > /opt/tomcat/webapps/host-manager/META-INF/context.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<Context antiResourceLocking="false" privileged="true" >
+  <!-- <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" /> -->
+</Context>
+EOF_CONTEXT_H
+
+
+echo ""
+echo "################################################################################"
+echo "#######                       Setup setenv.sh                            #######"
+echo "#######      Sets different environment variables read by Tomcat         #######"
+echo "################################################################################"
+
+sudo cat <<- EOF_SETENV > /opt/tomcat/bin/setenv.sh
+# export JPDA_OPTS="-agentlib:jdwp=transport=dt_socket, address=9999, server=y, suspend=n"
+export CATALINA_OPTS="-agentlib:jdwp=transport=dt_socket,address=9999,server=y,suspend=n"
+EOF_SETENV
 
 
 echo ""
@@ -62,7 +122,7 @@ echo "##########################################################################
 
 sudo cat <<- EOF > /etc/systemd/system/tomcat.service
  [Unit]
- Description=Apache Tomcat Web Application Container
+ Description=Apache Tomcat Web Applicatiprivilegedon Container
  After=network.target
 
  [Service]
